3d3c0b76fd6bdd4b0eb59d3c2641ec8c6b5d923d
yjgaia <yjdev88@gmail.com>
2025-02-24 12:44:02 +0900

    Implement KaiaNFTDisplay component with image loading functionality and add SupportedCollections enum

--- src/nft/KaiaNFTDisplay.ts
+++ src/nft/KaiaNFTDisplay.ts
@@ -1,9 +1,29 @@
-import { DomNode } from "@common-module/app";
+import { DomNode, el } from "@common-module/app";
+import { AppCompConfig } from "@common-module/app-components";
+import MateOnchainImagesContract from "../contracts/MateOnchainImagesContract.js";
+import SupportedCollections from "./SupportedCollections.js";
 
-class KaiaNFTDisplay extends DomNode {
-  constructor() {
+export default class KaiaNFTDisplay extends DomNode {
+  constructor(
+    private collection: SupportedCollections,
+    private tokenId: bigint,
+  ) {
     super(".kaia-nft-display");
+    this.loadImage();
+  }
+
+  private async loadImage() {
+    const loadingSpinner = new AppCompConfig.LoadingSpinner().appendTo(this);
+
+    let imageSrc: string | undefined;
+    if (this.collection === SupportedCollections.DogeSoundClubMates) {
+      imageSrc = await MateOnchainImagesContract.getImage(this.tokenId);
+    }
+
+    if (imageSrc && imageSrc !== "") {
+      this.append(el("img", { src: imageSrc }));
+    }
+
+    loadingSpinner.remove();
   }
 }
-
-export default new KaiaNFTDisplay();
---
+++ src/nft/SupportedCollections.ts
@@ -0,0 +1,12 @@
+enum SupportedCollections {
+  DogeSoundClubMates,
+  DogeSoundClubEMates,
+  DogeSoundClubBiasedMates,
+  SigorSparrows,
+  SigorHouseDeeds,
+  KCDKongz,
+  KCDPixelKongz,
+  BabyPing,
+}
+
+export default SupportedCollections;
