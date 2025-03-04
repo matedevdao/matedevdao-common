bdd24832cc593cd4edc3636a48a62e23d3529633
yjgaia <yjdev88@gmail.com>
2025-03-04 12:21:30 +0900

    Remove ParsingNFTDataContract and update index exports

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,3 @@
-export { default as ParsingNFTDataContract } from "./contracts/ParsingNFTDataContract.js";
 export { default as MDDModuleConfig } from "./MDDModuleConfig.js";
 export { default as HoldingNFTList } from "./nft-holder/HoldingNFTList.js";
 export { default as KaiaNFTDisplay } from "./nft/KaiaNFTDisplay.js";
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-export { default as ParsingNFTDataContract } from "./contracts/ParsingNFTDataContract.js";
 export { default as MDDModuleConfig } from "./MDDModuleConfig.js";
 export { default as HoldingNFTList } from "./nft-holder/HoldingNFTList.js";
 export { default as KaiaNFTDisplay } from "./nft/KaiaNFTDisplay.js";
 export { default as SupportedCollections } from "./nft/SupportedNFTAddresses.js";
-
