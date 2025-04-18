b9890695164c5fdae3dc3e709221dee687f14475
yjgaia <yjdev88@gmail.com>
2025-04-18 15:47:19 +0900

    Refactor MDDModuleConfig to import AppCompConfig and MaterialLoadingSpinner from @commonmodule, and update related logic

--- src/MDDModuleConfig.ts
+++ src/MDDModuleConfig.ts
@@ -1,9 +1,13 @@
+import { AppCompConfig } from "@commonmodule/app-components";
+import { MaterialLoadingSpinner } from "@commonmodule/material-loading-spinner";
 import { KaiaWalletModuleConfig } from "kaia-wallet-module";
 
 const MATEDEVDAO_WALLETCONNECT_PROJECT_ID = "647df3b69553d6f1261ea1482bd90d4a";
 
 class MDDModuleConfig {
   public init(options: { appName: string }) {
+    AppCompConfig.LoadingSpinner = MaterialLoadingSpinner;
+
     KaiaWalletModuleConfig.init({
       ...options,
       walletConnectProjectId: MATEDEVDAO_WALLETCONNECT_PROJECT_ID,
