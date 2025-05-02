149520b2dddb0a1169a8b78e64231330a18b459d
yjgaia <yjdev88@gmail.com>
2025-05-02 13:27:28 +0900

    Add custom font and styles for Sigor Sparrow preview
    - Introduced a new font 'neodgm' with a web font source.
    - Created styles for the Sigor Sparrow preview component, including positioning and font settings for dialogue elements.

--- lib/babyping/BabyPingPreview.d.ts
+++ lib/babyping/BabyPingPreview.d.ts
@@ -1,6 +1,8 @@
 import { DomNode } from "@commonmodule/app";
 import NFTData from "../nft/NFTData.js";
 export default class BabyPingPreview extends DomNode {
+    private gameScreen;
     constructor(nftData: NFTData);
+    private updateGameScreenSize;
 }
 //# sourceMappingURL=BabyPingPreview.d.ts.map
--- lib/sigor-sparrows/SigorSparrowPreview.d.ts
+++ lib/sigor-sparrows/SigorSparrowPreview.d.ts
@@ -1,6 +1,8 @@
 import { DomNode } from "@commonmodule/app";
 import NFTData from "../nft/NFTData.js";
 export default class SigorSparrowPreview extends DomNode {
+    private gameScreen;
     constructor(nftData: NFTData);
+    private updateGameScreenSize;
 }
 //# sourceMappingURL=SigorSparrowPreview.d.ts.map
