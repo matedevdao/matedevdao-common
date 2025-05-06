7090313c461c8a38f98f8ebfdf65392901a5119f
yjgaia <yjdev88@gmail.com>
2025-05-06 17:00:31 +0900

    feat: Add NFTDataWithMeta and NFTEditForm exports to index files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export { default as MDDModuleConfig } from "./MDDModuleConfig.js";
 export { default as getNFTEditForm } from "./nft/getNFTEditForm.js";
 export { default as HoldingNFTList } from "./nft/HoldingNFTList.js";
+export { default as NFTDataWithMeta } from "./nft/NFTDataWithMeta.js";
 export { default as NFTDisplay } from "./nft/NFTDisplay.js";
+export { default as NFTEditForm } from "./nft/NFTEditForm.js";
+
